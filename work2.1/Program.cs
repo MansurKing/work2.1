f1470b56d2374d1be8e68b843afc21f148fcb21a
MansurKing <102825225+MansurKing@users.noreply.github.com>
2022-04-06 17:47:21 +0300

    init

---
+++ work2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace work2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRepetitions;
+            string messages;
+
+            Console.Write($"Введите слово, которое будет зацикленно: ");
+            messages = Console.ReadLine();
+            Console.Write($"Введите кол-во повторений: ");
+            numberOfRepetitions = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberOfRepetitions; i++)
+            {
+                Console.WriteLine(messages);
+            }
+        }
+    }
+}
